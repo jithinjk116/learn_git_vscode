1b597786ef1c492a8c7500b79c2594dc53f799aa
jithinjkumar15 <jithinjkumar15@gmail.com>
2025-10-28 17:18:18 +0530

    Updated learn_python.py and learn_sql.sql

--- learn_sql.sql
+++ learn_sql.sql
@@ -1,2 +1,5 @@
 create Table IF NOT EXISTS LEARN_JITHIN as
 SELECT * from LRNJJK where id > 2 and id is not null
+
+create Table IF NOT EXISTS my-jjkN as
+SELECT * from LRNJJK where id > 2 and id is not null
